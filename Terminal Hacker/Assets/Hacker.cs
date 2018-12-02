f572f6982c2eec80e8e39344dd1f8f7baa89925e
Deonna Hodges <dnhodges@gmail.com>
2018-12-02 12:39:17 -0800

    added ability to read user input.

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hacker : MonoBehaviour {
+public class Hacker : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        ShowMainMenu("Hello, Deonna\n");
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnUserInput(string input)
+    {
+        print("The user typed in: " + input);
+    }
+
+    private static void ShowMainMenu(string greeting)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack into?\n");
+        Terminal.WriteLine("Press E for Equifax");
+        Terminal.WriteLine("Press F for Facebook");
+        Terminal.WriteLine("Press R for The Rock University");
+        Terminal.WriteLine("\nEnter your selection: ");
+    }
 }
